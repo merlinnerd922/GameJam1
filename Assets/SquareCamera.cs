c4a02d5c6356c2b1006f66828764232a6a896653
chen_solomon <richardb.chen@utoronto.ca>
2015-01-10 15:11:20 -0500

    overhead camera

---
+++ Assets/SquareCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquareCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Upon loading all assets, use this to adjust the width of the camera.
+	void Awake(){
+	
+        Vector2 screenRes = new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight);
+		float planeScale = 30.0f;
+		MeshFilter plane = GetComponent(typeof(MeshFilter)) as MeshFilter;
+
+		Vector3 v3 = plane.transform.localScale;
+		Debug.Log("Image screen size: " + v3.x  + " x " + v3.y); 
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
